de026d09b14dbe7e9e9f688796aee3d901a74bf7
Marina Monsivais <minamonsi@me.com>
2022-08-23 21:41:42 -0500

    Created header

--- satyaluna-app/src/components/Header.js
+++ satyaluna-app/src/components/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
 import SatyalunaLogo from "../../src/images/SatyalunaLogo.png"
-import ClearOceanHeader from "../images/ClearOceanHeader.png"
 
 function Header(){
     return(
-        <header className="satyaluna-header">
+        <div className="satyaluna-header">
+            <header className="header-elements">
             <img className="header-logo" src={SatyalunaLogo} alt="Satyaluna logo">
             </img>
             <h1>Satyaluna</h1>
-        </header>
+            </header>
+        </div>
     )
 }
 
